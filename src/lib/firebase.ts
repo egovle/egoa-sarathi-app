27f05e376f095a76f960454d43e21ee9522d7554
Aditya Patkar <nuteniqspl@gmail.com>
2025-06-26 17:29:19 +0000

    the details are already there

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAVWezupcCQhE6FhdxSgsD1SVPxtjDK72w",
   authDomain: "egoasarthi.firebaseapp.com",
   projectId: "egoasarthi",
-  storageBucket: "egoasarthi.appspot.com",
+  storageBucket: "egoasarthi.firebasestorage.app",
   messagingSenderId: "582450828090",
   appId: "1:582450828090:web:a0ed05ea1a74710230f603",
   measurementId: "G-9ZGSFH8X1F"
