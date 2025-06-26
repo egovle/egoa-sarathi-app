e0ba429f6bfa87573822f1cbcc76d83fabc55bd4
Aditya Patkar <nuteniqspl@gmail.com>
2025-06-26 17:01:35 +0000

    I never created a project on google cloud console. You never told me to

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,11 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
+// Stripped down to essentials to avoid configuration errors.
 const firebaseConfig = {
   apiKey: "AIzaSyAVWezupcCQhE6FhdxSgsD1SVPxtjDK72w",
   authDomain: "egoasarthi.firebaseapp.com",
   projectId: "egoasarthi",
-  storageBucket: "egoasarthi.appspot.com",
-  messagingSenderId: "582450828090",
-  appId: "1:582450828090:web:a0ed05ea1a74710230f603",
-  measurementId: "G-9ZGSFH8X1F"
 };
 
 // Initialize Firebase
