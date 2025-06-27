855e8fb8965f2019d4db50b74a61cbc32bb3ab54
Aditya Patkar <nuteniqspl@gmail.com>
2025-06-27 13:58:34 +0000

    give the rules also we started firestore database with test mode. We may

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,17 +2,20 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAVWezupcCQhE6FhdxSgsD1SVPxtjDK72w",
   authDomain: "egoasarthi.firebaseapp.com",
   projectId: "egoasarthi",
+  storageBucket: "egoasarthi.appspot.com",
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { app, db, auth };
+export { app, db, auth, storage };
