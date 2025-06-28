17709332ed0a75bc4d40d0bd4cc6ae35c93fb319
Aditya Patkar <nuteniqspl@gmail.com>
2025-06-28 21:23:11 +0000

    Cross origin request detected from 3000-firebase-studio-1750946492758.cl

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // This is required to allow the Next.js dev server to be accessed from the Project IDX preview window.
+    allowedDevOrigins: [
+        'https://3000-firebase-studio-1750946492758.cluster-zumahodzirciuujpqvsniawo3o.cloudworkstations.dev',
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
