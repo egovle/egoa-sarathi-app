8eabff3ec255357bc7f0d7b028488dba161af0cd
Aditya Patkar <nuteniqspl@gmail.com>
2025-07-26 15:24:18 +0000

    still failing. How do we resolve this issue?
    also why do i have to manu

---
+++ src/next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  output: 'standalone',
+  experimental: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+  },
+};
+
+module.exports = nextConfig;
